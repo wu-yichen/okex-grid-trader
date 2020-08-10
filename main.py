63b1937de557c1dc579fa58f985afff8cc19f314
wu-yichen <wuyichen1987@gmail.com>
2020-08-10 12:21:51 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,156 @@
+import ccxt
+import pymongo
+import time
+
+# 定义交易所
+
+okex = ccxt.okex({
+    'apiKey': '',
+    'secret': '',
+    'password': ''
+})
+
+# 定义mongodb
+
+myClient = pymongo.MongoClient("mongodb://localhost:27017")
+db = myClient["OKEX"]
+collection = db["EOS/USDT"]
+collection.drop()
+
+# 定义下单参数
+# buy
+
+fee_rate = 0.0015
+
+order_symbol = 'EOS/USDT'
+order_type = 'limit'
+order_side = 'buy'
+order_amount = 0.10016
+
+# 获取EOS最新价格
+
+eos_last = okex.fetch_ticker(order_symbol)['last']
+
+print("EOS 最新价格", eos_last)
+
+order_price = eos_last * order_amount * 10 - 0.005
+
+# fee_rate = okex.calculate_fee(order_symbol,order_type,order_side,order_amount,order_price,'taker')['rate']
+# set by exchange
+order_fee = order_amount * fee_rate
+
+usdt_balance = okex.fetch_free_balance()['USDT']
+
+final_order = order_price * order_amount
+
+num = 2
+while num > 0 and usdt_balance > final_order:
+    take_order = okex.create_order(
+        order_symbol, order_type, order_side, order_amount, order_price)
+
+    post = {"单价": order_price,
+            "订单号": take_order['id'],
+            "方向": take_order['side']}
+    x = collection.insert_one(post)
+    print("数据库插入完成")
+
+    eos_last = okex.fetch_ticker(order_symbol)['last']
+    order_price = eos_last * order_amount * 10 - 0.05
+    final_order = order_price * order_amount
+    usdt_balance = okex.fetch_balance()['free']['USDT']
+
+    num -= 1
+    time.sleep(1)
+
+# online_order = okex.fetch_open_orders(order_symbol)
+
+# for order in online_order:
+#     # 存储订单信息
+#     post = {"单价": order['price'],
+#             "订单号": order['id'],
+#             "方向": order['side'],
+#             "手续费": order_fee}
+#     x = collection.insert_one(post)
+
+# print("数据库插入完成")
+
+while 1:
+    for i in collection.find():
+        takeorder_id = i['订单号']
+        takeorder_side = i['方向']
+        takeorder_price = i['单价']
+        takeorder_fee = order_fee
+        print("*" * 10)
+
+        # 获取 eos 最新价格
+        eos_last = okex.fetch_ticker(order_symbol)['last']
+        print("eos 最新价格", eos_last)
+
+        print("orderid", takeorder_id)
+
+        # 查询订单状态
+        order_status = okex.fetch_order_status(takeorder_id, order_symbol)
+        print(order_status)
+
+        # 如果买单成交
+        if order_status == "closed" and takeorder_side == "buy":
+
+            # 定义卖单价格
+            sell_side = "sell"
+            cost = (1 + takeorder_fee) * takeorder_price
+            sell_fee = cost * fee_rate
+            sell_price = cost + sell_fee + 0.05
+
+            sell_amount = order_amount - order_fee
+
+            if eos_last > sell_price:
+                # 下卖单
+                sell_price = eos_last
+                # take_sell_order = okex.create_order(
+                #     order_symbol, order_type, sell_side, sell_amount, sell_price)
+
+            take_sell_order = okex.create_order(
+                order_symbol, order_type, sell_side, sell_amount, sell_price)
+
+            # 卖单信息存入数据库
+            print("开始写入数据库")
+            post = {
+                "单价": sell_price,
+                "订单号": take_sell_order['id'],
+                "方向": take_sell_order['side'],
+            }
+
+            collection.insert_one(post)
+
+            # 删除已完成订单
+            query = {"订单号": takeorder_id}
+            collection.delete_one(query)
+            print("已删除完成订单， 订单号为:", takeorder_id)
+
+            time.sleep(3)
+
+        elif order_status == "closed" and takeorder_side == "sell":
+            buy_side = "buy"
+            buy_price = eos_last * order_amount * 10 - 0.05
+
+            take_buy_order = okex.create_order(
+                order_symbol, order_type, buy_side, order_amount, buy_price)
+
+            # 卖单信息存入数据库
+            print("开始写入数据库")
+            post = {
+                "单价": buy_price,
+                "订单号": take_buy_order['id'],
+                "方向": take_buy_order['side']}
+
+            collection.insert_one(post)
+
+            # 删除已完成订单
+            query = {"订单号": takeorder_id}
+            collection.delete_one(query)
+            print("已删除完成订单， 订单号为:", takeorder_id)
+
+            time.sleep(3)
+        else:
+            print("订单处于其他状态")
+            time.sleep(1)
---
+++ test.py
@@ -0,0 +1,142 @@
+from pprint import pprint
+
+import ccxt
+import pymongo
+import time
+
+# 定义交易所
+okex = ccxt.okex({
+    'apiKey': '80f17ce1-5b2e-4d34-85ee-180bf442db3e',
+    'secret': 'F63903B0BBB041EFC1BF4AF4B24EF466',
+    'password': 'Yuukiji1747'
+})
+
+# 定义mongodb
+myClient = pymongo.MongoClient("mongodb://localhost:27017")
+mydb = myClient["OKEX"]
+mycol = mydb["EOSUSDT"]
+mycol.drop()
+
+# 定义下单参数
+order_symbol = 'EOS/USDT'
+order_type = 'limit'
+order_side = 'sell'
+order_amount = 0.1  # 单笔交易 = order_amout * order_price
+
+# 获取EOS最新价格
+
+eos_last = okex.fetch_ticker(order_symbol)['last']
+print("EOS 最新价格", eos_last)
+
+order_price = eos_last - 0.01  # 添加手续费 0.1%
+
+# order_price = eos_last * 1.00015
+
+
+# 需求： 下num个订单， 循环监控订单状态， 如果买单成交， 则下一个卖单， 如果卖单成交， 就下一个买单
+num = 3
+
+while num > 0:
+
+    eos_balance = okex.fetch_balance()['free']['USDT']
+
+
+    if eos_balance >= 1:
+
+        take_order = okex.create_order(
+            order_symbol, order_type, order_side, order_amount, order_price)
+
+        pprint(take_order)
+
+        order_price -= 0.01
+
+        eos_balance = okex.fetch_balance()['free']['USDT']
+
+        print("当前balance", eos_balance)
+        print("*" * 20)
+    else:
+        print("余额不足, 小于 1.0: ", eos_balance)
+
+    num -= 1
+    time.sleep(1)
+
+# aa = okex.fetch_balance()['EOS']['free']
+
+online_order = okex.fetch_open_orders(order_symbol)
+
+# pprint(online_order)
+
+for m in online_order:
+    # 存储订单信息
+    mydict = {"单价": m['price'],
+              "订单号": m['id'], "方向": m['side']}
+    x = mycol.insert_one(mydict)
+print("数据库插入完成")
+
+exit()
+
+while 1:
+    for y in mycol.find():
+        takeorder_id = y['订单号']
+        takeorder_side = y['方向']
+        takeorder_price = y['单价']
+        print("*" * 10)
+
+        # 获取 eos 最新价格
+        eos_last = okex.fetch_ticker(order_symbol)['last']
+        print("eos 最新价格", eos_last)
+
+        print("orderid", takeorder_id)
+
+        # 查询订单状态
+        order_status = okex.fetch_order_status(takeorder_id, order_symbol)
+        print(order_status)
+
+        # 如果买单成交
+        if order_status == "canceled" and takeorder_side == "buy":
+
+            # 定义卖单价格
+            sell_side = "sell"
+
+            sell_price = takeorder_price + 0.5  # shouxufei
+
+            # 下卖单
+            take_sell_order = okex.create_order(
+                order_symbol, order_type, sell_side, order_amount, sell_price)
+
+            # 卖单信息存入数据库
+            print("开始写入数据库")
+            mydict = {
+                "单价": take_sell_order['price'], "订单号": take_sell_order['id'], "方向": take_sell_order['side']}
+
+            mycol.insert_one(mydict)
+
+            # 删除已完成订单
+            myquery = {"订单号": takeorder_id}
+            mycol.delete_one(myquery)
+            print("已删除完成订单， 订单号为:", takeorder_id)
+
+            time.sleep(3)
+
+        elif order_status == "canceled" and takeorder_side == "sell":
+            buy_side = "buy"
+            buy_price = eos_last - 0.08
+
+            take_buy_order = okex.create_order(
+                order_symbol, order_type, buy_side, order_amount, buy_price)
+
+            # 卖单信息存入数据库
+            mydict = {
+                "单价": take_buy_order['price'], "订单号": take_buy_order['id'], "方向": take_buy_order['side']}
+
+            mycol.insert_one(mydict)
+
+            # 删除已完成订单
+            myquery = {"订单号": takeorder_id}
+            mycol.delete_one(myquery)
+            print("已删除完成订单， 订单号为:", takeorder_id)
+
+            time.sleep(3)
+        else:
+            print("订单处于其他状态")
+            time.sleep(1)
